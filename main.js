82f1c8fadfb649b2878d50485bc5f93847913483
Malena Pchersky <malena.pechersky@gmail.com>
2023-12-18 10:47:45 -0300

    se realizan propmts iniciales

---
+++ main.js
@@ -0,0 +1,34 @@
+
+
+
+let persona = prompt("Hola!, ya sos cliente?");
+
+if(persona =='si'){
+    
+    alert("Bienvenido nuevamente");
+}
+       
+else {
+    alert("Te damos la bienvanida a Capp Café");
+}
+
+
+let nombre = prompt("Ingrese su nombre y apellido")
+alert("El nombre que ingresaste es " + nombre )
+
+
+let sucursal = prompt("En qué sucursal querés reservar")
+    if (sucursal == "Palermo")
+        alert("Reservaste en Suc. Palermo");
+    else if(sucursal == "Urquiza")
+        alert("Reservaste en Suc. Urquiza");
+    else if(sucursal == "Olivos")
+        alert("Reservaste en Suc. Urquiza");
+    else{
+        alert("No tenemos sucursal en ese barrio, estamos en Palermo, Urquiza y Olivos")
+    }
+        
+
+
+
+
