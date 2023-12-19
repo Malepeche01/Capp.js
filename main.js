e0aa54bc363ffe196b3d5ce095659ed5578be709
Malena Pchersky <malena.pechersky@gmail.com>
2023-12-18 22:11:50 -0300

    solo if else

--- main.js
+++ main.js
@@ -1,40 +1,36 @@
-let reservar = true
-
-do{
-    in_reserva = prompt("Gracias por elegirnos, querés hacer una reserva?");
-    if(in_reserva == "si") {
-        let sucursal = prompt("En qué sucursal querés reservar?")
+in_reserva = prompt("Gracias por elegirnos, querés hacer una reserva?");
+if (in_reserva == "si") {
+    let sucursal = prompt("En qué sucursal querés reservar?")
+    
+    if (sucursal.toLowerCase() == "palermo"){
+        alert("Reservaste en Suc. Palermo");
+    }
         
-        if (sucursal == "Palermo"){
-            alert("Reservaste en Suc. Palermo");}
-            
-        else if(sucursal == "Urquiza"){
-            alert("Reservaste en Suc. Urquiza");}
-            
-         else if(sucursal == "Olivos"){
-            alert("Reservaste en Suc. Olivos"); }
-            
-         else{
-            alert("No tenemos sucursal en ese barrio, estamos en Palermo, Urquiza y Olivos")
-            break}
-        }
-              
-let nombre = prompt("Ingrese su nombre y apellido");
+    else if(sucursal.toLowerCase() == "urquiza"){
+        alert("Reservaste en Suc. Urquiza");
+    }
+        
+    else if(sucursal.toLowerCase() == "olivos"){
 
-if (nombre !=""){
-    alert("El nombre que ingresaste es " + nombre )
-    reservar = false
-}
-else{
-    alert("Debe ingresar un nombre para poder realizar la reserva")
-    break
-}
+        alert("Reservaste en Suc. Olivos"); 
+    }
+        
+    else{
+        alert("No tenemos sucursal en ese barrio, estamos en Palermo, Urquiza y Olivos")
+    }
 
-     
-
+    let nombre = prompt("Ingrese su nombre y apellido");
+    if (nombre !=""){
+        alert("El nombre que ingresaste es " + nombre )
+    }
+    else{
+        alert("Debe ingresar un nombre para poder realizar la reserva")
+    }    
 }
-while(reservar)        
- 
+else {
+    alert("Morite entonces");
+}   
+
 
 
 
