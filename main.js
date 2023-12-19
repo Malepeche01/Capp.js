2041cda0b24a52f7e006281dce445ee9297e6050
Malena Pchersky <malena.pechersky@gmail.com>
2023-12-18 21:50:40 -0300

    proyecto sin funciones

--- main.js
+++ main.js
@@ -1,33 +1,40 @@
+let reservar = true
 
-
-
-let persona = prompt("Hola!, ya sos cliente?");
-
-if(persona =='si'){
-    
-    alert("Bienvenido nuevamente");
-}
-       
-else {
-    alert("Te damos la bienvanida a Capp Café");
-}
-
-
-let nombre = prompt("Ingrese su nombre y apellido")
-alert("El nombre que ingresaste es " + nombre )
-
-
-let sucursal = prompt("En qué sucursal querés reservar")
-    if (sucursal == "Palermo")
-        alert("Reservaste en Suc. Palermo");
-    else if(sucursal == "Urquiza")
-        alert("Reservaste en Suc. Urquiza");
-    else if(sucursal == "Olivos")
-        alert("Reservaste en Suc. Urquiza");
-    else{
-        alert("No tenemos sucursal en ese barrio, estamos en Palermo, Urquiza y Olivos")
-    }
+do{
+    in_reserva = prompt("Gracias por elegirnos, querés hacer una reserva?");
+    if(in_reserva == "si") {
+        let sucursal = prompt("En qué sucursal querés reservar?")
         
+        if (sucursal == "Palermo"){
+            alert("Reservaste en Suc. Palermo");}
+            
+        else if(sucursal == "Urquiza"){
+            alert("Reservaste en Suc. Urquiza");}
+            
+         else if(sucursal == "Olivos"){
+            alert("Reservaste en Suc. Olivos"); }
+            
+         else{
+            alert("No tenemos sucursal en ese barrio, estamos en Palermo, Urquiza y Olivos")
+            break}
+        }
+              
+let nombre = prompt("Ingrese su nombre y apellido");
+
+if (nombre !=""){
+    alert("El nombre que ingresaste es " + nombre )
+    reservar = false
+}
+else{
+    alert("Debe ingresar un nombre para poder realizar la reserva")
+    break
+}
+
+     
+
+}
+while(reservar)        
+ 
 
 
 
