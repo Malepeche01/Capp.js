1cf676dbed48c275aca1822a4a7d282d86a5179f
Malena Pchersky <malena.pechersky@gmail.com>
2023-12-23 15:06:02 -0300

    reservas de sucursales y pedido de info para cursos

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-in_reserva = prompt("Gracias por elegirnos, querés hacer una reserva?");
+const sucursales = ['Urquiza' , ' Palermo' , ' y Olivos' ]
+
+
+let in_reserva = prompt("Gracias por elegirnos, querés hacer una reserva? Estamos en " + sucursales)
 if (in_reserva == "si") {
-    let sucursal = prompt("En qué sucursal querés reservar?")
+    let sucursal = prompt("En qué sucursal querés reservar?");
     
     if (sucursal.toLowerCase() == "palermo"){
         alert("Reservaste en Suc. Palermo");
@@ -19,17 +22,36 @@ if (in_reserva == "si") {
         alert("No tenemos sucursal en ese barrio, estamos en Palermo, Urquiza y Olivos")
     }
 
-    let nombre = prompt("Ingrese su nombre y apellido");
+    let nombre = prompt("Ingresá tu nombre y apellido");
     if (nombre !=""){
         alert("El nombre que ingresaste es " + nombre )
     }
     else{
         alert("Debe ingresar un nombre para poder realizar la reserva")
-    }    
+    }   
+    
+
 }
 else {
-    alert("Morite entonces");
-}   
+    
+    const cursos =  ['1. Barista y Arte latte ', '2. Arte latte avanzado ','3. Tostado de café ','4. Cocktelería con café ']
+    let hacerCurso = prompt("Te gustaría formarte como barista?");
+    if (hacerCurso == "si") 
+        alert("Estos son los cursos que estamos ofreciendo actualmente" + cursos)
+    for (const curso of cursos){
+        console.log(curso)
+    }
+    let info = prompt("Si querés que te enviemos info sobre los cursos, dejanos tu mail en este casillero")
+    console.log(info);
+    if (info =="" || info == null) {
+        alert("Será la próxima!")
+    }
+    
+    else {
+        alert("Te estaremos enviando la info enseguida")
+    }        
+}  
+
 
 
 
